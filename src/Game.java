73258125392427d6e64418fa66ab941e38be81a3
madamson8 <matthewadamson8@gmail.com>
2015-11-11 21:35:00 -0700

    Got second level, lose screen, win screen, and AI all working.  got text set up and working.  started creation of a manifest file.  ready to go.  just needs a story, more platforms/enemies.  All base code is in place.

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,8 @@
 /**
- * Created by darkp_000 on 11/2/2015.
+ * Created by Matthew on 11/2/2015.
+ * credit to Me, Jaylen, and Izeni.
+ * Feel free to use if you want, just
+ * credit us.
  */
 
 import javax.swing.*;
@@ -10,16 +13,19 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 @SuppressWarnings("serial")
-public class Game extends JPanel implements KeyListener, MouseListener{
+class Game extends JPanel implements KeyListener, MouseListener{
 
     boolean exit = false; // Booleans
     boolean wallCollision = false;
     boolean secHasPassed = false;
     boolean options = false;
     boolean buttonOne = false;
-    boolean levelInGame = false;
+    boolean levelInGame = true;
     boolean gravity = true;
     boolean running = true;
+    boolean secondLevelInGame = false;
+    boolean invincibleMode = false;
+    boolean hasWon = false;
 
     boolean canMoveL = false;
     boolean canMoveR = false;
@@ -61,6 +67,10 @@ public class Game extends JPanel implements KeyListener, MouseListener{
     int goomY = wallY-30;
     int goomSize = 20;
 
+    int cEnemySize = 15;
+    int cEnemyX = 0;
+    int cEnemyY = 0;
+
     // Long
     long time = System.currentTimeMillis();
 
@@ -89,6 +99,8 @@ public class Game extends JPanel implements KeyListener, MouseListener{
             game.repaint();
             game.customUpdate();
             game.backAndForthEnemy();
+            game.complexEnemy();
+
             timer.setText(String.valueOf(game.secondsPassed));
             if(System.currentTimeMillis() > game.time + 1000 && !game.secHasPassed) {
                 game.time = System.currentTimeMillis();
@@ -113,7 +125,7 @@ public class Game extends JPanel implements KeyListener, MouseListener{
             g.fillRect(charX, charY, 20, 30); // Character
 
             g.setColor(Color.BLACK); // wall
-            g.fillRect(wallX, wallY, 800, 10);
+            g.fillRect(wallX, wallY, WIN_WIDTH, 10);
 
             // Platforms
             g.fillRect(platOneX, platOneY, platOneW, platOneH);
@@ -124,15 +136,65 @@ public class Game extends JPanel implements KeyListener, MouseListener{
             g.fillRect(goomX,goomY,goomSize,goomSize);
         }
 
+        if(secondLevelInGame && !levelInGame) {
+            g.setColor(Color.RED);
+            g.fillRect(0,0,WIN_WIDTH,WIN_HEIGHT); // Fills window
+
+            g.setColor(Color.BLACK); // Draws floor
+            g.fillRect(wallX, wallY, WIN_WIDTH, 10);
+
+            g.setColor(Color.BLACK);
+            g.fillRect(platOneX,platOneY,platOneW,platOneH); // draws platforms
+
+            g.setColor(Color.GREEN); // Character
+            g.fillRect(charX,charY,charW,charH);
+
+            g.setColor(Color.BLUE);
+            g.fillRect(cEnemyX,cEnemyY,cEnemySize,cEnemySize);
+        }
+
         if(options) {
             g.setColor(Color.BLACK);
             g.fillRect(0,0,800,700); // Window
             g.setColor(Color.RED);
             g.fillRect(buttOneX,buttOneY,50,50);
         }
+        if(!running) {
+            char[] gameOver = {'G','a','m','e',' ','O','v','e','r'};
+            g.setColor(Color.RED);
+            g.fillRect(0,0,WIN_WIDTH,WIN_HEIGHT);
+
+            g.setColor(Color.BLACK);
+            g.drawChars(gameOver, 0, gameOver.length,WIN_WIDTH/2,WIN_HEIGHT/2);
+//            g.drawChars(char[] data, int offset, int length, int x, int y) ex of text
+        }
+        char[] quit = {'Q','u','i','t','?'};
+        char[] restart = {'R','e','s','t','a','r','t','?'};
+
+        if(hasWon) {
+            char[] youWin = {'Y','o','u',' ','H','a','v','e',' ','W','o','n','!'};
+            g.setColor(Color.WHITE);
+            g.fillRect(0,0,WIN_WIDTH,WIN_HEIGHT);
+
+            g.setColor(Color.CYAN);
+            g.fillRect(WIN_WIDTH/3,WIN_HEIGHT/2+30,50,50);
+            g.fillRect(WIN_WIDTH/3+WIN_WIDTH/3,WIN_HEIGHT/2+30,50,50);
+
+            g.setColor(Color.BLACK); // strings last because they need to override everything else
+            g.drawChars(youWin, 0, youWin.length, WIN_WIDTH/2-20, WIN_HEIGHT/2);
+            g.drawChars(quit, 0, quit.length, WIN_WIDTH/3+12,WIN_HEIGHT/2+55);
+            g.drawChars(restart, 0, restart.length, WIN_WIDTH/3+WIN_WIDTH/3-restart.length+10, WIN_HEIGHT/2+55);
+
+        }
     }
 
+    //TODO copy to top
+    int quitButtonX = WIN_WIDTH/3;
+    int quitButtonY = WIN_HEIGHT/2+30;
+    int quitButtonSize = 50;
+
     public void customUpdate() {  // Remember, all logic goes in here
+
         if(charX > 780) {
             charX = 0;
         }
@@ -157,79 +219,92 @@ public class Game extends JPanel implements KeyListener, MouseListener{
             numOfJumps = 0;
             secHasPassed = false;
         }
+        if(levelInGame) {
+            if (charY < platOneY &&
+                    charX < platOneX + platOneW && // Collision for plat 1 and so on
+                    charX + charW > platOneX &&
+                    charY < platOneY + platOneH &&
+                    charH + charY > platOneY) { //TODO  Get it so that it stops him when he hits the bottom
+                charY = platOneY - 30;
+                numOfJumps = 0;
+                charVY = 0;
+                wallCollision = true; // Gets top of the platform collision
+            }
 
-        if(charY < platOneY &&
-                charX < platOneX + platOneW && // Collision for plat 1 and so on
-                charX + charW > platOneX &&
-                charY < platOneY + platOneH &&
-                charH + charY > platOneY) { //TODO  Get it so that it stops him when he hits the bottom
-            charY = platOneY-30;
-            numOfJumps = 0;
-            charVY = 0;
-            wallCollision = true; // Gets top of the platform collision
+            if (charY > platOneY &&
+                    charX < platOneX + platOneW && // Collision for plat 1 and so on
+                    charX + charW > platOneX &&
+                    charY < platOneY + platOneH &&
+                    charH + charY > platOneY) {
+                charY = platOneY + platOneH + 1;
+                numOfJumps = maxJumps;
+                charVY = 0;
+                wallCollision = true; // Gets bottom of platform collision
+            }
+
+            if (charY > platTwoY &&
+                    charX < platTwoX + platTwoW &&
+                    charX + charW > platTwoX &&
+                    charY < platTwoY + platTwoH &&
+                    charH + charY > platTwoY) {
+                charY = platTwoY + platTwoH + 1;
+                numOfJumps = maxJumps;
+                charVY = 0;
+                wallCollision = true;
+                System.out.println("Bottom");
+            }
+
+            if (charY < platTwoY &&
+                    charX < platTwoX + platTwoW &&
+                    charX + charW > platTwoX &&
+                    charY < platTwoY + platTwoH &&
+                    charH + charY > platTwoY) {
+                charY = platTwoY - 30;
+                numOfJumps = 0;
+                charVY = 0;
+                wallCollision = true;
+            }
+
+            if (charX < platThreeX + platThreeW &&
+                    charX + charW > platThreeX &&
+                    charY < platThreeY + platThreeH &&
+                    charH + charY > platThreeY) {
+                charY = platThreeY - 30;
+                numOfJumps = 0;
+                charVY = 0;
+                wallCollision = true;
+                secondLevelInGame = true; // Level 2
+                levelInGame = false; // Level 1
+            }
+
+            if (charX < goomX + goomSize &&
+                    charX + charW > goomX &&
+                    charY < goomY + goomSize &&
+                    charH + charY > goomY) {
+                gameOver();
+            }
         }
-
-        if(charY > platOneY &&
-                charX < platOneX + platOneW && // Collision for plat 1 and so on
-                charX + charW > platOneX &&
-                charY < platOneY + platOneH &&
-                charH + charY > platOneY) { //TODO  Get it so that it stops him when he hits the bottom
-            charY = platOneY+platOneH+1;
-            numOfJumps = maxJumps;
-            charVY = 0;
-            wallCollision = true; // Gets bottom of platform collision
+        if(secondLevelInGame) { // Level 2 logic
+            if(charX < platOneX + platOneW &&
+                    charX + charW > platOneX &&
+                    charY < platOneY + platOneH &&
+                    charH + charY > platOneY) {
+//                charY = platOneY - charH;
+//                numOfJumps = 0;
+//                charVY = 0;
+//                wallCollision = true;
+                hasWon = true;
+            }
         }
-
-        if(charY > platTwoY &&
-                charX < platTwoX + platTwoW &&
-                charX + charW > platTwoX &&
-                charY < platTwoY + platTwoH &&
-                charH + charY > platTwoY) {
-            charY = platTwoY+platTwoH+1;
-            numOfJumps = maxJumps;
-            charVY = 0;
-            wallCollision = true;
-            System.out.println("Bottom");
-        }
-
-        if(charY < platTwoY &&
-                charX < platTwoX + platTwoW &&
-                charX + charW > platTwoX &&
-                charY < platTwoY + platTwoH &&
-                charH + charY > platTwoY) {
-            charY = platTwoY-30;
-            numOfJumps = 0;
-            charVY = 0;
-            wallCollision = true;
-        }
-
-        if(charX < platThreeX + platThreeW &&
-                charX + charW > platThreeX &&
-                charY < platThreeY + platThreeH &&
-                charH + charY > platThreeY) {
-            charY = platThreeY-30;
-            numOfJumps = 0;
-            charVY = 0;
-            wallCollision = true;
-        }
-
-        if(canMoveL) { // Movement Logic
+        if (canMoveL) { // Movement Logic
             charX -= 5;
         }
-        if(canMoveR) {
+        if (canMoveR) {
             charX += 5;
         }
-        if(charVY < -15) {
+        if (charVY < -15) {
             charVY = -15;
         }
-
-        if(charX < goomX + goomSize &&
-                charX + charW > goomX &&
-                charY < goomY + goomSize &&
-                charH + charY > goomY) {
-            gameOver();
-        }
-
     }
 
     public void gameOver() {
@@ -248,26 +323,54 @@ public class Game extends JPanel implements KeyListener, MouseListener{
         boolean goomR = false;
         boolean goomL = true;
         int second = 0;
-        if(running) {
+        if(running && !invincibleMode) {
             goomX += 3;
-//            long time = System.currentTimeMillis();
-//            if(time > time+1000) {
-//                time = System.currentTimeMillis();
-//                second += 1;
-//
-//            }
             if(goomX > WIN_WIDTH) {
                 goomX = 0;
             }
         }
+        if(!running) {
+            goomX = charX+charW+50;
+        }
 
+    }
+
+    private void complexEnemy() {
+        int cEnemySpeed = 2;
+        if(secondLevelInGame) {
+            if(running && !invincibleMode) {
+                if (charX > cEnemyX) { // If you are to the right of the enemy, it moves to the right
+                    cEnemyX += cEnemySpeed;
+                }
+                if (charX < cEnemyX) { // If you are to the left of the enemy, it moves to the left.
+                    cEnemyX -= cEnemySpeed;
+                }
+                if(charY < cEnemyY) {
+                    cEnemyY -= cEnemySpeed;
+                }
+                if(charY > cEnemyY) {
+                    cEnemyY += cEnemySpeed;
+                }
+            }
+            if(running && // Only activates if you are in the second level and you are running.
+                    charX < cEnemyX + cEnemySize &&
+                    charX + charW > cEnemyX &&
+                    charY < cEnemyY + cEnemySize &&
+                    charH + charY > cEnemyY) {
+                gameOver(); // Calls game over
+            }
+            if(!running) {
+                cEnemyX = WIN_WIDTH/2;
+                cEnemyY = 0;
+            }
+        }
     }
 
     public void keyTyped(KeyEvent e) {
 
     }
 
-    public void keyPressed(KeyEvent e) {  // Logic can be used in these
+    public void keyPressed(KeyEvent e) {  // Logic can be used in these but try to shy away from it
 //        System.out.println(e); // Enable to see what a keycode is.  Remember to disable
         switch (e.getKeyCode()) {
             case 37:
@@ -292,11 +395,8 @@ public class Game extends JPanel implements KeyListener, MouseListener{
                 charY += 5;
                 break;
             case 27: // Escape
+                running = false;
                 options = true;
-                if (options) {
-                    levelInGame = true;
-                    options = false;
-                }
                 break;
             case 32: // Space
                 System.out.println(charX);
@@ -306,16 +406,24 @@ public class Game extends JPanel implements KeyListener, MouseListener{
                 break;
             case 87: // W
                 gravity = false;
+                invincibleMode = true;
                 charY -= 1;
                 break;
             case 65: // A
                 charX -= 1;
+                gravity = false;
+                invincibleMode = true;
                 break;
             case 68: // D
                 charX += 1;
+                gravity = false;
+                invincibleMode = true;
                 break;
             case 82: // R
                 running = true;
+                hasWon = false;
+                gravity = true;
+                invincibleMode = false;
                 break;
         }
 
